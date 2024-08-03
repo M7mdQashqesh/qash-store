d701d6b6f25efb2742013ae01012289fc0cf488a
Mohammed Qashqesh <mohammedbelal601@gmail.com>
2024-08-03 19:03:45 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleList() {
+  var list = document.getElementById("modal");
+  if (list.classList.contains("hidden")) {
+    list.classList.remove("hidden");
+  } else {
+    list.classList.add("hidden");
+  }
+}
