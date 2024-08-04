5872f86b20a3bfa9eac18d01a4ddb4cd137b56bd
Mohammed Qashqesh <mohammedbelal601@gmail.com>
2024-08-04 21:58:18 +0300

    convert language to arabic (rtl)

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function toggleList() {
+const toggleList = () => {
   var list = document.getElementById("modal");
   if (list.classList.contains("hidden")) {
     list.classList.remove("hidden");
   } else {
     list.classList.add("hidden");
   }
-}
+};
